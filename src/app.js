73016d7e09e59e987203ee0b79fd9bb74282e54b
Michael Caintic De Leon <delemic@americas.manulife.net>
2021-09-07 09:40:34 +0800

    Stage 3 complete

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { composeP } = require('ramda');
 const port = 3000;
 
 const app = express();
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res){
-  res.render('index',{title:'index'});
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8')
+const users = JSON.parse(userData);
+console.log(accounts);
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Account Summary', accounts: accounts });
+})
+
+app.get('/savings', function (req, res) {
+  res.render('account', { account: accounts.savings });
+})
+
+app.get('/checking', function (req, res) {
+  res.render('account', { account: accounts.checking });
+})
+
+app.get('/credit', function (req, res) {
+  res.render('account', { account: accounts.credit });
+})
+
+app.get('/profile', function (req, res) {
+  res.render('profile', { user: users[0]});
 })
 
 app.listen(port, () => {
