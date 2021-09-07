3e1d74bcd463ec06c78f661e16043f4bb5a4313c
Michael Caintic De Leon <delemic@americas.manulife.net>
2021-09-07 11:10:38 +0800

    stage4 completed

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@ const { composeP } = require('ramda');
 const port = 3000;
 
 const app = express();
+const router = express.Router();
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
 const accounts = JSON.parse(accountData);
@@ -33,7 +36,30 @@ app.get('/credit', function (req, res) {
 })
 
 app.get('/profile', function (req, res) {
-  res.render('profile', { user: users[0]});
+  res.render('profile', { user: users[0] });
+})
+
+app.get('/transfer', function (req, res) {
+  res.render('transfer', {});
+})
+
+app.post('/transfer', function (req, res) {
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf8');
+  res.render('transfer', { message: "Transfer Completed" });
+})
+
+app.get('/payment', function (req, res) {
+  res.render('payment', {account: accounts.credit});
+})
+app.post('/payment', function (req, res) {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf8');
+  res.render('payment', {  message: "Payment Successful", account: accounts.credit });
 })
 
 app.listen(port, () => {
