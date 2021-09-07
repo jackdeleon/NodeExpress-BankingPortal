009ed34a5bde20c268a61e6193eb6561e1351705
Michael Caintic De Leon <delemic@americas.manulife.net>
2021-09-07 11:47:20 +0800

    All Complete!

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const { accounts, users, writeJSON } = require('./data');
 const app = express();
 const router = express.Router();
 
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -19,41 +22,11 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'Account Summary', accounts: accounts });
 })
 
-app.get('/savings', function (req, res) {
-  res.render('account', { account: accounts.savings });
-})
-
-app.get('/checking', function (req, res) {
-  res.render('account', { account: accounts.checking });
-})
-
-app.get('/credit', function (req, res) {
-  res.render('account', { account: accounts.credit });
-})
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', function (req, res) {
   res.render('profile', { user: users[0] });
-})
-
-app.get('/transfer', function (req, res) {
-  res.render('transfer', {});
-})
-
-app.post('/transfer', function (req, res) {
-  accounts[req.body.from].balance -= req.body.amount;
-  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-  writeJSON();
-  res.render('transfer', { message: "Transfer Completed" });
-})
-
-app.get('/payment', function (req, res) {
-  res.render('payment', {account: accounts.credit});
-})
-app.post('/payment', function (req, res) {
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount);
-  writeJSON();
-  res.render('payment', {  message: "Payment Successful", account: accounts.credit });
 })
 
 app.listen(port, () => {
