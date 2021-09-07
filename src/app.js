0291bb6e32fba876b0b2b24d828f0f7cb1e37335
Michael Caintic De Leon <delemic@americas.manulife.net>
2021-09-07 11:23:57 +0800

    Stage 5 Complete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
 const { composeP } = require('ramda');
 const port = 3000;
+const { accounts, users, writeJSON } = require('./data');
 
 const app = express();
 const router = express.Router();
@@ -12,12 +14,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
-
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('src/json/users.json', 'utf8')
-const users = JSON.parse(userData);
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Account Summary', accounts: accounts });
@@ -46,8 +42,7 @@ app.get('/transfer', function (req, res) {
 app.post('/transfer', function (req, res) {
   accounts[req.body.from].balance -= req.body.amount;
   accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('transfer', { message: "Transfer Completed" });
 })
 
@@ -57,8 +52,7 @@ app.get('/payment', function (req, res) {
 app.post('/payment', function (req, res) {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('payment', {  message: "Payment Successful", account: accounts.credit });
 })
 
